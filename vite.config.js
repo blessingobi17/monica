e172b12c3d673157f204b37de828ff9588f78d3f
Blessing Obi <bjoseph863@gmail.com>
2024-07-19 16:18:40 +0100

    upload updated

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/monica-website/",
+  base: "/monica/",
   plugins: [react()],
+  server: {
+    port: 4000,
+  },
 });
